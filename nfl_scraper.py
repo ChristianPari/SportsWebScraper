9f0e5b18c65002aa2c26020f026f274744718501
Christian Pari <christianpari@outlook.com>
2021-03-22 21:14:49 -0400

    Revamped the ability to find a players position by creation of another auto browser and scraping the players specific url, script runs with creation of all teams own csv file

--- nfl_scraper.py
+++ nfl_scraper.py
@@ -1,79 +1,88 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import datetime
 import pandas
 import re
 
-path = 'C:/Users/Chris/AppData/Local/Programs/Python/Python39/geckodriver'
-firefox_options = Options()
-firefox_options.add_argument("-private")
-# firefox_options.headless = True
-browser = webdriver.Firefox(executable_path=path, options=firefox_options)
-url = 'https://www.pro-football-reference.com/'
-local_data = {}
+def run():
+  path = 'C:/Users/Chris/AppData/Local/Programs/Python/Python39/geckodriver'
+  firefox_options = Options()
+  firefox_options.add_argument("-private")
+  firefox_options.headless = True
+  browser = webdriver.Firefox(executable_path=path, options=firefox_options)
+  url = 'https://www.pro-football-reference.com/'
+  local_data = {}
 
-browser.get(url)
-browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/ul[1]/li[2]/a').click()
+  browser.get(url)
+  browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/ul[1]/li[2]/a').click()
 
-table = browser.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[2]/table/tbody')
-rows = table.find_elements_by_css_selector('tr')
-links = []
+  table = browser.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[2]/table/tbody')
+  rows = table.find_elements_by_css_selector('tr')
+  links = []
 
-for row in rows:
-  if row.get_attribute('class') == '':
-    elm = row.find_element_by_css_selector('th')
-    link = elm.find_element_by_tag_name('a').get_attribute('href')
-    links.append(link)
+  for row in rows:
+    if row.get_attribute('class') == '':
+      elm = row.find_element_by_css_selector('th')
+      link = elm.find_element_by_tag_name('a').get_attribute('href')
+      links.append(link)
 
-for link in links:
-  data_frame = pandas.DataFrame(columns=['Team', 'Number', 'Name', 'Position'])
+  for link in links:
+    data_frame = pandas.DataFrame(columns=['Team', 'Number', 'Name', 'Position'])
 
-  team_abrv = link[-4:-1]
-  local_data[team_abrv] = []
+    team_abrv = link[-4:-1]
+    local_data[team_abrv] = []
 
-  browser.get(link + '2020_roster.htm')
-  roster_table = browser.find_element_by_id('games_played_team')
-  roster_header = roster_table.find_element_by_tag_name('thead').find_element_by_tag_name('tr')
-  headings = roster_header.find_elements_by_tag_name('th')
-  for heading in headings:
-    if heading.get_attribute('aria-label') == 'Uniform number':
-      heading.click()
+    nfl_start_month = 9
+    cur_month = datetime.date.today().month
+    cur_year = datetime.date.today().year
 
-  player_rows = []
-  player_rows.insert(0, roster_table.find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')[0])
-  for player_row in player_rows:
-    player_data = []
-    player_object = {}
+    if nfl_start_month > cur_month:
+      cur_year -= 1
 
-    if player_row.get_attribute('class') == '':
-      player_object['Team'] = team_abrv
+    browser.get(link + str(cur_year) + '_roster.htm')
+    roster_table = browser.find_element_by_id('games_played_team')
+    roster_header = roster_table.find_element_by_tag_name('thead').find_element_by_tag_name('tr')
+    headings = roster_header.find_elements_by_tag_name('th')
+    for heading in headings:
+      if heading.get_attribute('aria-label') == 'Uniform number':
+        heading.click()
 
-      data = player_row.find_elements_by_tag_name('td')
+    player_rows = []
+    player_rows.insert(0, roster_table.find_element_by_tag_name('tbody').find_elements_by_tag_name('tr'))
+    for player_row in player_rows:
+      player_data = []
+      player_object = {}
 
-      uniform_number = player_row.find_element_by_tag_name('th').text
-      player_data.insert(0, uniform_number)
-      player_object['Number'] = uniform_number
+      if player_row.get_attribute('class') == '':
+        player_object['Team'] = team_abrv
 
-      name = data[0].text
-      filtered_name = re.sub('[^a-zA-Z\\s]', '', name).strip()
-      player_data.insert(1, filtered_name)
-      player_object['Name'] = filtered_name
+        data = player_row.find_elements_by_tag_name('td')
 
-      position = ''
-      if data[2].text == '':
-        player_link = data[0].find_element_by_tag_name('a').get_property('href')
-        player_browser = webdriver.Firefox(executable_path=path, options=firefox_options)
-        player_browser.get(player_link)
-        player_pos = player_browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div[2]/p[2]').text
-        player_browser.close()
-        position = player_pos.split(':')[1]
+        uniform_number = player_row.find_element_by_tag_name('th').text
+        player_data.insert(0, uniform_number)
+        player_object['Number'] = uniform_number
 
-      else:
-        position = data[2].text.upper()
+        name = data[0].text
+        filtered_name = re.sub('[^a-zA-Z\\s]', '', name).strip()
+        player_data.insert(1, filtered_name)
+        player_object['Name'] = filtered_name
 
-      player_data.insert(2, position)
-      player_object['Position'] = position
+        position = ''
+        if data[2].text == '':
+          player_link = data[0].find_element_by_tag_name('a').get_property('href')
+          player_browser = webdriver.Firefox(executable_path=path, options=firefox_options)
+          player_browser.get(player_link)
+          player_pos = player_browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div[2]/p[2]').text
+          player_browser.close()
+          position = player_pos.split(':')[1]
 
-      local_data[team_abrv].insert((++0), player_data)
-      data_frame = data_frame.append(player_object, ignore_index=True)
+        else:
+          position = data[2].text.upper()
 
-  data_frame.to_csv('/Users/Chris/Documents/csv-files/' + team_abrv + '.csv')
+        player_data.insert(2, position)
+        player_object['Position'] = position
+
+        local_data[team_abrv].insert((++0), player_data)
+        data_frame = data_frame.append(player_object, ignore_index=True)
+
+    data_frame.to_csv('/Users/Chris/Documents/csv-files/nfl/' + team_abrv + '.csv')
